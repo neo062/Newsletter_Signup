1ed55dd7ed6b5b8626caac76a7bf4dac20a9b3a5
navin <navinkumar241999@gmail.com>
2023-01-23 21:45:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+
+
+
+app.listen(3000,function(){
+  console.log("server is running on Port 3000");
+});
